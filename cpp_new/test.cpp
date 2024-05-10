90f898773fb4bf103b0a0deb68d3f6af3cee965a
haschkt <tobias.haschke@voith.com>
2024-05-10 19:51:50 +0200

    Write

--- cpp_new/test.cpp
+++ cpp_new/test.cpp
@@ -5,6 +5,21 @@
 
 #include "hsm.hpp"
 
+class HsmTest : public Hsm
+{
+    int foo;
+
+protected:
+    State s1;
+    State s2;
+
+public:
+    HsmTest();
+    Msg const *topHndlr(Msg const *msg);
+    Msg const *s1Hndlr(Msg const *msg);
+    Msg const *s2Hndlr(Msg const *msg);
+};
+
 int main()
 {
     printf("Hi c++\n");
