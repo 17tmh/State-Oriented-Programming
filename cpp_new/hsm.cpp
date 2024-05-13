4325dda242b8377588bbc77a9371a9e9d4a59e70
haschkt <tobias.haschke@voith.com>
2024-05-13 12:14:12 +0200

    Update

--- cpp_new/hsm.cpp
+++ cpp_new/hsm.cpp
@@ -1,1 +1,9 @@
 #include "hsm.hpp"
+
+State::State(char const *n, State *s, EvtHndlr h) : name(n), super(s), hndlr(h)
+{
+}
+
+Hsm::Hsm(char const *n, EvtHndlr t) : name(n), top("top", nullptr, t)
+{
+}
--- cpp_new/test.cpp
+++ cpp_new/test.cpp
@@ -20,11 +20,48 @@ public:
     Msg const *s2Hndlr(Msg const *msg);
 };
 
+Msg const *HsmTest::topHndlr(Msg const *msg)
+{
+    return msg;
+}
+
+Msg const *HsmTest::s1Hndlr(Msg const *msg)
+{
+    return msg;
+}
+
+Msg const *HsmTest::s2Hndlr(Msg const *msg)
+{
+    return msg;
+}
+
+// HsmTest::HsmTest()
+//     : Hsm("myhsm", HsmTest::topHndlr),
+//       s1("s1", &top, HsmTest::s1Hndlr),
+//       s1("s2", &top, HsmTest::s2Hndlr)
+// {
+//     foo = 0;
+// }
+
+// HsmTest::HsmTest()
+//     : Hsm("HsmTest", static_cast<EvtHndlr>(&HsmTest::topHndlr)),
+//       s1("s1", &top, static_cast<EvtHndlr>(&HsmTest::s1Hndlr)),
+//       s11("s11", &s1, static_cast<EvtHndlr>(&HsmTest::s11Hndlr)),
+//       s2("s2", &top, static_cast<EvtHndlr>(&HsmTest::s2Hndlr)),
+//       s21("s21", &s2, static_cast<EvtHndlr>(&HsmTest::s21Hndlr)),
+//       s211("s211", &s21, static_cast<EvtHndlr>(&HsmTest::s211Hndlr))
+// {
+//     foo = 0;
+// }
+
 int main()
 {
     printf("Hello, World!\n");
 
     std::cout << "Hi, C++!" << std::endl;
+
+    State s("blablub", nullptr, nullptr);
+    Hsm h("lll2", nullptr);
 
     return 0;
 }
